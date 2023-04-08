7c535a1d91e4578e5f58d8c68fecad5e4c9d73b5
Alexander Kukoba <luckychel@gmail.com>
2023-04-09 02:05:20 +0300

    убрал повторяющийся код

--- XO-game/Game/Controllers/MainViewController.swift
+++ XO-game/Game/Controllers/MainViewController.swift
@@ -14,32 +14,40 @@ class MainViewController: UIViewController {
     //MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
     
+    override func performSegue(withIdentifier identifier: String, sender: Any?) {
+        if shouldPerformSegue(withIdentifier: identifier, sender: sender) {
+            super.performSegue(withIdentifier: identifier, sender: sender)
+        }
+    }
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        if identifier == "segaToGame" {
+            guard let sender = sender, sender as? GameMode != nil, let mode = sender as? GameMode else {
+                return false
+            }
+            let vc: GameViewController = UIStoryboard(name: "Main", bundle: nil)
+                .instantiateViewController(withIdentifier: "GameViewController") as! GameViewController
+            vc.gameMode = mode
+            self.navigationController?.pushViewController(vc, animated: true)
+            return false
+        }
+        return true
+    }
     
     //MARK: Button Actions
     @IBAction func TwoHuman(_ sender: UIButton) {
-        let vc: GameViewController = UIStoryboard(name: "Main", bundle: nil)
-            .instantiateViewController(withIdentifier: "GameViewController") as! GameViewController
-        vc.gameMode = .withHuman
-        self.navigationController?.pushViewController(vc, animated: true)
+        performSegue(withIdentifier: "segaToGame", sender: GameMode.withHuman)
     }
     
     @IBAction func HumanAi(_ sender: UIButton) {
-        let vc: GameViewController = UIStoryboard(name: "Main", bundle: nil)
-            .instantiateViewController(withIdentifier: "GameViewController") as! GameViewController
-        vc.gameMode = .withAI
-        self.navigationController?.pushViewController(vc, animated: true)
+        performSegue(withIdentifier: "segaToGame", sender: GameMode.withAI)
     }
     
     
     @IBAction func BlindGame(_ sender: UIButton) {
-        let vc: GameViewController = UIStoryboard(name: "Main", bundle: nil)
-            .instantiateViewController(withIdentifier: "GameViewController") as! GameViewController
-        vc.gameMode = .blindGame
-        self.navigationController?.pushViewController(vc, animated: true)
+        performSegue(withIdentifier: "segaToGame", sender: GameMode.blindGame)
     }
-    
-
 }
