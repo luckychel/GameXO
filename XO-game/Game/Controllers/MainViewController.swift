63ca4c86676c5b19d4acf753dbe0bad859842b26
Alexander Kukoba <luckychel@gmail.com>
2023-04-09 01:11:11 +0300

    готово

--- XO-game/Game/Controllers/MainViewController.swift
+++ XO-game/Game/Controllers/MainViewController.swift
@@ -10,9 +10,7 @@ import UIKit
 
 class MainViewController: UIViewController {
 
-    private let vc: GameViewController = UIStoryboard(name: "Main", bundle: nil)
-        .instantiateViewController(withIdentifier: "GameViewController") as! GameViewController
-    
+
     //MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,17 +20,23 @@ class MainViewController: UIViewController {
     
     //MARK: Button Actions
     @IBAction func TwoHuman(_ sender: UIButton) {
+        let vc: GameViewController = UIStoryboard(name: "Main", bundle: nil)
+            .instantiateViewController(withIdentifier: "GameViewController") as! GameViewController
         vc.gameMode = .withHuman
         self.navigationController?.pushViewController(vc, animated: true)
     }
     
     @IBAction func HumanAi(_ sender: UIButton) {
+        let vc: GameViewController = UIStoryboard(name: "Main", bundle: nil)
+            .instantiateViewController(withIdentifier: "GameViewController") as! GameViewController
         vc.gameMode = .withAI
         self.navigationController?.pushViewController(vc, animated: true)
     }
     
     
     @IBAction func BlindGame(_ sender: UIButton) {
+        let vc: GameViewController = UIStoryboard(name: "Main", bundle: nil)
+            .instantiateViewController(withIdentifier: "GameViewController") as! GameViewController
         vc.gameMode = .blindGame
         self.navigationController?.pushViewController(vc, animated: true)
     }
